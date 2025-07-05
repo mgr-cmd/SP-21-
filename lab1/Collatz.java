28b3f83c0ac1c693dc3cf7c56e79e8350c03bd55
mgr-cmd <[samiralemagar@outlook.com]>
2025-07-06 01:22:33 +0530

    done setting up the done with Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -4,18 +4,25 @@
 public class Collatz {
 
     /** Buggy implementation of nextNumber! */
-    public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
-        } else {
-            return n * 2;
-        }
-    }
+    public static int nextNumber(int a) {
+        if ((a%2)==0){
+           
+	       	return a/2 ;
+        
+	}else if((a%2)!=0){
+            
+		return ((3*a)+1) ;
+        
+	}else if(a==1){
+            
+		return a ; 
+        } 
+	return 0 ; 
+    } 
+    
 
     public static void main(String[] args) {
-        int n = 5;
+        int n = 3;
         System.out.print(n + " ");
         while (n != 1) {
             n = nextNumber(n);
@@ -23,5 +30,6 @@ public class Collatz {
         }
         System.out.println();
     }
-}
+   }
+
 
